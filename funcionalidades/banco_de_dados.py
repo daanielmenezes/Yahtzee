e23290bfa63aa8e272d2188709ed9a302f8f5a73
Daniel Menezes <danielmenezes42@gmail.com>
2020-05-19 23:01:54 -0300

    correção especificação

--- funcionalidades/banco_de_dados.py
+++ funcionalidades/banco_de_dados.py
@@ -20,7 +20,7 @@ __all__ = ["abre_acesso", "fecha_acesso" ]
 ##########################################################################
 #  Abre uma conexão com o banco de dados
 #  
-#  Retorna um dicionario [ conexao, cursor ] que poderão ser usados para 
+#  Retorna um dicionario { 'conexao', 'cursor' } que poderão ser usados para 
 #  executar comandos sql e deverão ser fechados pela função fecha_acesso
 #  ou retorna 1 caso haja erro de conexão com o mysql.
 #
@@ -46,14 +46,17 @@ def abre_acesso():
 #########################################################
 #  Fecha a conexão com o banco e o cursor
 #  
-#  conexao: uma conexão criada pela abre_acessor
+#  acesso: um dicionario do tipo { 'conexao', 'cursor' }
+#  conexao: uma conexão criada pela abre_acesso
 #  cursor: um cursor criado pela abre_acesso
 #
 #  Retorna 0 em caso de sucesso
 #  ou retorna 1 em caso de erro.
 #  ou retorna 2 caso conexao ou cursor sejam invalidos
 #########################################################
-def fecha_acesso(conexao, cursor):
+def fecha_acesso(acesso):
+    conexao = acesso['conexao']
+    cursor = acesso['cursor']
     if type(conexao) != mysql.connector.connection.MySQLConnection or\
        type(cursor) != mysql.connector.cursor.MySQLCursorBufferedDict:
            return 2
