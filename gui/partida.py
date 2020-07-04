ef6c652e288a62f4decf2d699e516d109dcde625
Daniel Menezes <danielmenezes@aluno.puc-rio.br>
2020-07-04 00:22:05 -0300

    partida mostra os jogadores

--- gui/partida.py
+++ gui/partida.py
@@ -1,1 +1,17 @@
 
+from entidades import partida
+from tkinter import *
+
+def transicao( window ):
+    if window._frame:
+        window._frame.destroy()
+
+    window.config(cursor = 'arrow')
+
+    fr_partida = Frame(window)
+    fr_partida.pack()
+
+    jogadores = partida.obtem_info_partida()['jogadores']
+    for jogador in jogadores:
+        Label(fr_partida, text = jogador).pack()
+
