3097361d8487abe38e8a934c0b85ff818d9990b2
Daniel Menezes <danielmenezes@aluno.puc-rio.br>
2020-07-06 03:05:41 -0300

    botao de sair e alinhamento dos botões

--- gui/menu_principal.py
+++ gui/menu_principal.py
@@ -11,13 +11,20 @@ def transicao( window ):
     lb_title = Label(fr_menuPrincipal, text = "Yahtzee", fg = "black", font = "none 30 bold")
     lb_title.pack( pady = (10, 50))
 
-    bt_novaPartida = Button(fr_menuPrincipal, text = "Nova Partida", command = lambda: menu_partida.transicao(window))
-    bt_novaPartida.pack( pady = (5,5))
+    fr_botoes = Frame( fr_menuPrincipal )
+    fr_botoes.pack()
 
-    bt_novaPartida = Button(fr_menuPrincipal, text = "Continuar", command = lambda: continuar.transicao(window))
-    bt_novaPartida.pack( pady = (5,5))
+    bt_novaPartida = Button(fr_botoes, text = "Nova Partida", command = lambda: menu_partida.transicao(window))
+    bt_novaPartida.pack( pady = (5,5), fill = X )
 
-    bt_novaPartida = Button(fr_menuPrincipal, text = "Jogadores", command = lambda: menu_jogadores.transicao(window))
-    bt_novaPartida.pack( pady = (5,5))
+    bt_novaPartida = Button(fr_botoes, text = "Continuar", command = lambda: continuar.transicao(window))
+    bt_novaPartida.pack( pady = (5,5), fill = X)
+
+    bt_novaPartida = Button(fr_botoes, text = "Jogadores", command = lambda: menu_jogadores.transicao(window))
+    bt_novaPartida.pack( pady = (5,5), fill = X)
+
+    bt_sairDoJogo = Button( fr_botoes, text = "Sair do Jogo", command = lambda: window.destroy() )
+    bt_sairDoJogo.pack( pady = (5,5), fill = X)
+
 
     window._frame = fr_menuPrincipal
